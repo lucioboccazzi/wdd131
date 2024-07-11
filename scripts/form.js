ba289898b34deee967e17a28b9e82395c83b5c33
lucioboccazzi <lucioboccazzi@gmail.com>
2024-07-11 07:33:24 -0300

    l

---
+++ scripts/form.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      id: 'fc-1888',
+      name: "flux capacitor",
+      'avg-rating': 4.5
+    },
+    {
+      id: 'fc-2050',
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: 'fs-1987',
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: 'ac-2000',
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: 'jj-1969',
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+];
+
+function populateSelect() {
+    const selectElement = document.getElementById('product');
+
+    products.forEach(product => {
+
+        const option = document.createElement('option');
+
+        option.value = product.id;
+
+        option.textContent = product.name;
+
+        selectElement.appendChild(option);
+    });
+}
+
+populateSelect();
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
